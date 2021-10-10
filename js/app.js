0713b2336cb0fe4d3d502109aca8563356900943
Your Name <you@example.com>
2021-10-10 17:46:20 +0100

    Added js functionality to total order container in version 1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 const form =  document.querySelector(".calc__form");
-console.log(form);
 const products = form.querySelector ("#products");
 const orders = form.querySelector("#orders");
 const select = form.querySelector(".select__input");
@@ -14,12 +13,13 @@ const productSummary = summary.querySelector('li[data-id="products"]');
 const productQuantity = productSummary.querySelector(".item__calc");
 const productPriceTotal = productSummary.querySelector(".item__price");
 const productPrice = 5;
-
+let productSum = 0;
 
 const orderSummary = summary.querySelector('li[data-id="orders"]');
 const orderQuantity = orderSummary.querySelector(".item__calc");
 const orderPriceTotal = orderSummary.querySelector(".item__price");
 const orderPrice = 2;
+let orderSum = 0;
 
 const packageSummary = summary.querySelector('li[data-id="package"]');
 const packageType = packageSummary.querySelector(".item__calc");
@@ -27,39 +27,47 @@ const packagePriceTotal = packageSummary.querySelector(".item__price");
 const basicPrice = 0;
 const professionalPrice = 50;
 const premiumPrice = 70;
+let packagesValue = 0;
 
 const accountingSummary = summary.querySelector('li[data-id="accounting"]');
 const accountingPrice = accountingSummary.querySelector(".item__price");
-const accountingPriceValue = 35;
+let accountingPriceValue = 0;
 
 const terminalSummary = summary.querySelector('li[data-id="terminal"]');
 const terminalPrice = terminalSummary.querySelector(".item__price");
-const terminalPriceValue = 6;
+let terminalPriceValue = 0;
 
-// const totalOrderSummary = summary.querySelector("#total-price");
-// const totalPriceElem = totalOrderSummary.querySelector(".total__price");
-// let totalOrderPrice;
+const totalOrderSummary = summary.querySelector("#total-price");
+const totalOrderElem = totalOrderSummary.querySelector(".total__price");
+let totalOrderPrice = 0;
+
+function showTotal(){
+    totalOrderSummary.style.display = "flex";
+    totalOrderPrice = productSum + orderSum + packagesValue + accountingPriceValue + terminalPriceValue;
+    totalOrderElem.innerText = "$" + totalOrderPrice;
+}
 
  products.addEventListener("change", function (event) {
-
-     let productOrder = products.value * productPrice;
+     productSum = products.value * productPrice;
     productSummary.style.display = "flex";
     productQuantity.innerText = products.value + " * " + "$" + productPrice;
-    productPriceTotal.innerText ="$" + productOrder;
+    productPriceTotal.innerText ="$" + productSum;
 
+    showTotal();
 
  });
-
 
 orders.addEventListener("change", function(event) {
     orderSummary.style.display = "flex";
     orderQuantity.innerText = orders.value + " * " + "$" + orderPrice;
-    let orderValue = orders.value * orderPrice
-    orderPriceTotal.innerText = "$" + orderValue;
+    orderSum = orders.value * orderPrice
+    orderPriceTotal.innerText = "$" + orderSum;
+
+   showTotal();
+
 } );
 
 select.addEventListener("click", function (event) {
-   // console.log("select") ;
    dropdown.style.display = "block";
 });
 
@@ -70,38 +78,51 @@ packages.forEach(function (element) {
          packageSummary.style.display = "flex";
          packageType.innerText = this.innerText;
          if (this.innerText === "Basic") {
+             packagesValue = basicPrice;
              packagePriceTotal.innerText = "$"+ basicPrice;
          }
          if (this.innerText === "Professional") {
+             packagesValue = professionalPrice;
              packagePriceTotal.innerText = "$" + professionalPrice;
          }
          if (this.innerText === "Premium") {
+             packagesValue = premiumPrice;
              packagePriceTotal.innerText = "$" + premiumPrice;
          }
-         // else {
-         //     dropdown.style.display = "none";
-         // }
 
+         showTotal();
     });
 });
 
 accounting.addEventListener("change", function (event) {
-    accountingSummary.style.display = "flex";
-    accountingPrice.innerText = "$" + accountingPriceValue;
+
+    if (accounting.checked === true) {
+        accountingSummary.style.display = "flex";
+        accountingPriceValue = 35;
+        accountingPrice.innerText = "$" + accountingPriceValue;
+    }
 
     if (accounting.checked === false) {
         accountingSummary.style.display = "none";
+       accountingPriceValue = 0;
     }
-
+    showTotal();
 });
 
 terminal.addEventListener("change", function (event) {
-    terminalSummary.style.display = "flex";
-    terminalPrice.innerText = "$" + terminalPriceValue;
+
+    if (terminal.checked === true) {
+        terminalSummary.style.display = "flex";
+        terminalPriceValue = 6;
+        terminalPrice.innerText = "$" + terminalPriceValue;
+    }
 
     if (terminal.checked === false) {
         terminalSummary.style.display = "none";
+        terminalPriceValue = 0;
+        totalOrderSummary.style.display = "none";
     }
+    showTotal();
 
 });
 
